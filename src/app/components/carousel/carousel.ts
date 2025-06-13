65c9209ce2a86420f7cc84a8da861035447eb318
Sergio Rojas <sergiodev91@gmail.com>
2025-06-13 18:17:02 -0400

    optimize images

--- src/app/components/carousel/carousel.ts
+++ src/app/components/carousel/carousel.ts
@@ -5,8 +5,29 @@ import { LucideAngularModule } from 'lucide-angular';
   selector: 'app-carousel',
   imports: [LucideAngularModule],
   templateUrl: './carousel.html',
-  styles: ``
+  styles: ``,
 })
 export class Carousel {
-
+  public news = [
+    {
+      image: '/news-mock/news_1.webp',
+      title: 'Ferrari prepara mejoras clave para Baréin',
+      font: 'gpfans.com',
+    },
+    {
+      image: '/news-mock/news_2.webp',
+      title: 'Norris pide calma tras inicio de McLaren',
+      font: 'formula1.com',
+    },
+    {
+      image: '/news-mock/news_3.webp',
+      title: 'Mercedes es el segundo mejor, según Russell',
+      font: 'f1l.com',
+    },
+    {
+      image: '/news-mock/news_4.webp',
+      title: 'FIA stance on Checo angers Red Bull',
+      font: 'gpfans.com',
+    }
+  ];
 }
