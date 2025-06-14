880db98368331a937c2b5e397852c3a7dfb5b14e
Sergio Rojas <sergiodev91@gmail.com>
2025-06-14 17:18:52 -0400

    add loading login

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,23 +1,30 @@
 import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../auth.service';
+import { LucideAngularModule } from 'lucide-angular';
 import { Register } from '@app/auth/register/register';
+import { AuthService } from '@app/auth/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [Register],
+  imports: [LucideAngularModule, Register],
   templateUrl: './login.html',
   styles: ``,
 })
 export class Login {
   public showRegister = signal(false);
+  public isLoading = signal(false);
 
   private auth = inject(AuthService);
   private router = inject(Router);
 
   handleLogin() {
-    this.auth.login();
-    this.router.navigate(['/']);
+    this.isLoading.set(true);
+
+    setTimeout(() => {
+      this.auth.login();
+      this.isLoading.set(false);
+      this.router.navigate(['/']);
+    }, 1000);
   }
 
   handleToggleRegister() {
